b34c1fb4b81c85dd7c58d699b14074f9e30b320f
Adam Bird <adam.bird@gmail.com>
2012-03-06 12:45:25 +0000

    added test for multimap gpx file without elevation

--- spec/broutes/formats/gpx_track_spec.rb
+++ spec/broutes/formats/gpx_track_spec.rb
@@ -25,5 +25,31 @@ describe Formats::GpxTrack do
     it "sets the total descent" do
       @route.total_descent.round.should eq(37)
     end
+    
+    context "when file doesn't have elevation" do
+      before(:all) do
+        @file = open_file('single_lap_gpx_track_no_elevation.xml')
+        @target = Formats::GpxTrack.new
+        @route = GeoRoute.new
+
+        Formats::GpxTrack.new.load(@file, @route)
+      end
+
+      it "sets the start point lat" do
+        @route.start_point.lat.should eq(52.926467718938)
+      end
+      it "sets the start point lon" do
+        @route.start_point.lon.should eq(-1.216092432889)
+      end
+      it "sets the total distance" do
+        round_to(@route.total_distance, 3).should eq(123.955)
+      end
+      it "sets the total ascent" do
+        @route.total_ascent.should eq(0)
+      end
+      it "sets the total descent" do
+        @route.total_descent.should eq(0)
+      end
+    end
   end
 end
